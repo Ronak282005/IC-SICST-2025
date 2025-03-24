2a346bbbe9a6cd86c5a0db09b8a29d2f140f1112
ronak <ronakpraja340@gmail.com>
2025-03-24 11:47:26 +0530

    some code changes

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -60,13 +60,12 @@ const Footer = () => {
               </div>
             </Link>
             <p className="text-gray-300 text-xs">
+            The Microsoft CMT service was used for managing the peer-reviewing process for this conference. This service was provided for free by Microsoft and they bore all expenses, including costs for Azure cloud services as well as for software development and support.
+            </p>
+            <p className="text-gray-300 text-xs">
               Join us for the 1st International Conference on Sustainable & Intelligent Computing 
               in Science and Technology at PIET.
             </p>
-            {/* <p className="text-gray-300 text-xs">
-              Join us for the 1st International Conference on Sustainable & Intelligent Computing 
-              in Science and Technology at PIET.
-            </p> */}
           </div>
 
           {/* Contact Info */}
