2981922e1293b6f1a89f9a99cb73202762491cfb
ronak <ronakpraja340@gmail.com>
2025-05-14 23:54:54 +0530

    content changes

--- src/data/speakers.ts
+++ src/data/speakers.ts
@@ -2,7 +2,7 @@ export const speakers = [
     {
         "name": " Prof. (Dr.) Somitra Kumar Sanadhya",
         "designation": "Professor Department of CSE, IIT Jodhpur",
-        "image": "/gallery/keynote_speakers/Somitra.webp.png",
+        "image": "/gallery/keynote_speakers/Somitra.webp",
         "id": "1",
         "national": true
     },
